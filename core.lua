298c49e10ccb550aadbcbbe38344b6447a841ca5
missionz3r0 <missionz3r0@gmail.com>
2017-02-09 18:11:49 -0500

    Add the triggers path so we can require it.

--- core.lua
+++ core.lua
@@ -1,5 +1,8 @@
 dba = require("dba")
 
+package.path = package.path .. ";" .. getMudletHomeDir() .. "/scripts/Triggers/?.lua"
+
+require("_Triggers")
 require("Handlers")
 require("Skill")
 require("UI")
