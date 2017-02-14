5d600be48396623c27969a1c822c1c66c2a67106
missionz3r0 <missionz3r0@gmail.com>
2017-02-14 12:20:36 -0500

    Save set up
    Reworked save into the scripts finally. It’s no longer it’s own script
    and is now a handler event. All we need to do to save now is to send a
    saveEvent and everything that is added to the save handler will be
    saved.

---
+++ Handlers/Save_Handlers.lua
@@ -0,0 +1,38 @@
+registerAnonymousEventHandler("saveEvent", "Handlers.saveEventHandler")
+  local saveEventListeners = {}
+  function Handlers.addsaveEventListener(listenerName, functionToAdd)
+    saveEventListeners[listenerName] = functionToAdd
+  end
+
+  function Handlers.removesaveEventListener(listenerName)
+    saveEventListeners[listenerName] = nil
+  end
+
+  function Handlers.saveEventHandler()
+    local saveTable = {}
+    for k,v in pairs(saveEventListeners) do
+      saveTable[k] = v()
+    end
+    table.save(getMudletHomeDir().."/dartmud.txt", saveTable)
+  end
+
+registerAnonymousEventHandler("loadEvent", "Handlers.loadEventHandler")
+  local loadEventListeners = {}
+  function Handlers.addloadEventListener(listenerName, functionToAdd)
+    loadEventListeners[listenerName] = functionToAdd
+  end
+
+  function Handlers.removeloadEventListener(listenerName)
+    loadEventListeners[listenerName] = nil
+  end
+
+  function Handlers.loadEventHandler()
+    local saveTable = {}
+    if io.exists(getMudletHomeDir().."/dartmud.txt") then
+      
+      table.load(getMudletHomeDir().."/dartmud.txt", saveTable)
+      for k,v in pairs(loadEventListeners) do
+        v(saveTable[k])
+      end
+    end
+  end
