ccc79e6b97e5d5b4dd91b286ea5966ad248d2014
missionz3r0 <missionz3r0@gmail.com>
2017-03-26 21:49:43 -0400

    Blocking!
    Blocking has finally been re-added to the scripts. This makes sure a
    mage doesn’t muff a scroll when practice casting and the like.

---
+++ Scripts/Blocking.lua
@@ -0,0 +1,64 @@
+local Blocking = {}
+
+local sourceName = "blocking"
+
+local isBlocked = false
+local queue = {}
+
+local function block()
+  isBlocked = true
+end
+
+local function unblock()
+  if(isBlocked) then
+    local count = table.size(queue)
+    isBlocked = false
+    if count > 0 then
+      echo("\nProcessing "..count.." previously queued command(s).\n")
+      for i,v in ipairs(queue) do
+        send(v)
+      end
+    end
+    queue = {}
+  end
+end
+
+local function onNetworkOutput(args)
+  local command = args["command"]
+    if isBlocked then
+        echo("Queing command: '"..command.."'\n")
+        table.insert(queue, command)
+        denyCurrentSend()
+    end
+    -- these are what trigger blocking inputs
+    if string.match(command, "^cast ! ") == "cast ! " or
+        string.match(command, "^inscribe ") == "inscribe " or
+        string.match(command, "^invoke ") == "invoke " then
+        block()
+    end
+end
+
+local function load(args)
+  Events.addListener("unblockEvent",sourceName,unblock)
+  Events.addListener("blockEvent",sourceName,block)
+  Events.addListener("sysDataSendRequestEvent",sourceName,onNetworkOutput)
+end
+
+local function unload(args)
+  Events.removeListener("unblockEvent",sourceName)
+  Events.removeListener("blockEvent",sourceName)
+  Events.removeListener("sysDataSendRequestEvent",sourceName)
+end
+
+local function reload(args)
+  unload(args)
+  load(args)
+end
+
+Blocking = {
+  load = load
+  ,unload = unload
+  ,reload = reload
+}
+
+return Blocking
---
+++ Triggers/Blocking_Triggers.lua
@@ -0,0 +1,60 @@
+local Blocking = {}
+
+local triggers = {}
+local textToUnBlock = {
+  "You have written"
+  ,"The scroll writhes and disappears\\."
+  ,"A glowing rune appears on the"
+  ,"Your aura is too weak\\."
+  ,"corpse isn't here!"
+  ,"You don't have any mist!"
+  ,"You neglected to name a target\\."
+  ,"You don't know such a spell\\."
+  ,"No effect"
+  ,"What ?"
+  ,"Your concentration is disrupted and the spell is lost\\."
+  ,"Nothing happens"
+  ,"You don't have a scroll"
+  ,"The book is already obfuscated\\."
+  ,"No target specified."
+  ,"You need to be holding a blank scroll to inscribe\\."
+  ,"You don't have any \\.*!"
+  ,"You finish editing"
+  ,"You finish studying the book\\."
+  ,"You try to touch [A-Za-z]+, but you miss\\."
+}
+
+local function load(args)
+  local tempTriggers = {}
+
+  for i,v in ipairs(textToUnBlock) do
+    tempTriggers[i] =
+      tempRegexTrigger("^(?:> )?"..v.."[\\s\\S]*"
+                       ,[[
+                          local arguments = {}
+                          Events.raiseEvent("unblockEvent", arguments)
+                        ]])
+  end
+
+  triggers = tempTriggers
+end
+
+local function unload(args)
+  for i,v in pairs(triggers) do
+    killTrigger(v)
+  end
+  triggers = {}
+end
+
+local function reload(args)
+  load(args)
+  reload(args)
+end
+
+Blocking = {
+  load = load
+  ,unload = unload
+  ,reload = reload
+}
+
+return Blocking
