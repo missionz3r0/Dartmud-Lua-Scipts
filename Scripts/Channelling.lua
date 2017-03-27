7ebc258b2ba16b5d833ce8cae0927ba76c6f28d1
missionz3r0 <missionz3r0@gmail.com>
2017-03-26 21:51:14 -0400

    Channelling
    Should work as it did previously now.
    
    I do want to add in a historical log of what was channelled to, but
    that can wait till I have a place to save stuff to.

--- Scripts/Channelling.lua
+++ Scripts/Channelling.lua
@@ -4,21 +4,51 @@ local sourceName = "channelling"
 
 local currentChannelPower = 1
 local currentChannelTarget = ''
+local currentChannelCount = 0
 
 local function channel()
+  send("channel "..currentChannelPower.. " "..currentChannelTarget)
+  currentChannelCount = currentChannelCount + currentChannelPower
+  cecho("<yellow>Channelled "..currenChannelCount.." to "..currentChannelTarget.."\n")
 end
 
 local function channellingSetup(args)
   local power = args["power"]
   local target = args["target"]
+
+  currentChannelPower = tonumber(power)
+  currentChannelTarget = target
+  currentChannelCount = 0
+  Events.addListener("BEBTconcEvent", sourceName, channel)
+  send("conc")
+  cecho("<yellow>Channelling "..currentChannelPower.." to "..currentChannelTarget.."\n")
+end
+
+local function channellingCommands(args)
+  command = args["command"] or ''
+  if command == "off" then
+    Events.removeListener("BEBTconcEvent", sourceName)
+    cecho("<yellow>Channelling off")
+  elseif command == "resume" then
+    Events.addListener("BEBTconcEvent", sourceName, channel)
+    cecho("<yellow>Resuming channelling "..currentChannelPower.." to "..currentChannelTarget.."\n")
+
+  elseif command == "info" or command == "status" then
+    cecho("<yellow>Currently channelling "..currentChannelPower.." to "..currentChannelTarget.."\n")
+
+  else
+    cecho(command.." is invalid. Valid commands are off, resume, info, or status.")
+  end
 end
 
 local function load(args)
   Events.addListener("channellingEvent", sourceName, channellingSetup)
+    Events.addListener("channellingCommandEvent", sourceName, channellingCommands)
 end
 
 local function unload(args)
-  Handlers.removeListener("channellingEvent", sourceName)
+  Events.removeListener("channellingEvent", sourceName)
+    Events.removeListener("channellingCommandEvent", sourceName)
 end
 
 local function reload(args)
