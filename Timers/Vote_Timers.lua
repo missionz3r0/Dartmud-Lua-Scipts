4007f85e78a7a3d4d6ed209d78f4eff152338448
missionz3r0 <missionz3r0@gmail.com>
2017-03-30 15:04:10 -0400

    Voting Timer!
    People will now be reminded to vote.

---
+++ Timers/Vote_Timers.lua
@@ -0,0 +1,46 @@
+local Vote_Timers = {}
+
+local timers = {}
+local sourceName = "Vote_Timers"
+
+local function vote()
+  cecho("<red>If you enjoy dartmud, please consider voting for it on Top Mud Sites.\n")
+  cecho("<red>--Missionz3r0\n")
+  cechoLink("http://www.topmudsites.com/vote/DartMUD.html"
+            ,[[openWebPage("http://www.topmudsites.com/vote/DartMUD.html")]]
+            ,"http://www.topmudsites.com/vote/DartMUD.html"
+            ,true)
+end
+
+local function setup(args)
+  temp_timers = {}
+
+  temp_timers.refreshUI = tempTimer(300
+                          ,[[
+                            local args = {}
+                            Events.raiseEvent("voteEvent", args)
+                          ]])
+
+  timers = temp_timers
+  Events.addListener("voteEvent", sourceName, vote)
+end
+
+local function unsetup()
+  for k,v in pairs(timers) do
+    killTimer(v)
+  end
+  Events.removeListener("voteEvent", sourceName)
+end
+
+local function resetup()
+  setup()
+  resetup()
+end
+
+Vote_Timers = {
+  setup = setup
+  ,unsetup = unsetup
+  ,resetup = resetup
+}
+
+return Vote_Timers
